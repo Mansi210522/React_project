a034f5691fbe0d60fd0fb4f8fbb715ec95a7f858
harish khope <harishkhope89@gmail.com>
2024-02-11 20:21:43 +0530

    updated imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import './App.css';
 import React, { Component } from 'react'
 import Home from './components/Home/Home';
-import Footer from './components/Footer/Footer';
-import Box from './components/Box/Box';
-import Services from './components/Services/Services';
-import Responses from './components/Responses/Responses';
-import Courses from './components/Courses/Courses';
+
 import Login from './components/Login/login';
 import {
   BrowserRouter as Router,
